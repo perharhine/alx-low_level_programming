45ad74f219d16736c43725b13def4db97b6ec0e9
Paul Erharhine <perharhine@mail.com                                                                            git config --global user.email perharhine@mail.com>
2022-10-15 05:12:52 -0700

    positive or negative numbers

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * write alphabets in lowercase
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{putchar("%c", i);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/o-positive_or_negative.c
+++ 0x01-variables_if_else_while/o-positive_or_negative.c
@@ -2,32 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * main -  prints if the number is positive,negative or 0
+/** main - prints if the number is positive,negative or zero
  * Return: Always 0
- */
+ * /
 
 int main(void)
 
 {
 
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n",n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n)
+		printf("%d is zero\n",n);
 	}
-	else
+	else 
 	{
-		printf("%d is negative\n", n);
-	}
+		printf("%d is negative\n",n);
 
 	return (0);
 }
