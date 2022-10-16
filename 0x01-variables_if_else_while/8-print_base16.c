4e6f36b52b7c221673e1bb2b8575cbe19a35276f
Paul Erharhine <perharhine@mail.com                                                                            git config --global user.email perharhine@mail.com>
2022-10-16 11:55:53 -0700

    numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the numbers of base 16 in
+ * lowercase followed by new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+		putchar('\n');
+}
