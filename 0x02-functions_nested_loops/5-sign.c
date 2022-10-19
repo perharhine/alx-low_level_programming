599579dd1558357e793535a7e4468040cba9eab5
Paul Erharhine <perharhine@mail.com                                                                            git config --global user.email perharhine@mail.com>
2022-10-18 17:31:32 -0700

    sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: ther number of the sign to be printed
+ * Return: 1 if the number is greater than zero
+ * 0 if the number is zero
+ * -1 if the number is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+
+		_putchar('+');
+		return (1);
+}
