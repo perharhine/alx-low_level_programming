fb5fba2e8f8f0815d57f19393ca7cd93ba5156a1
Paul Erharhine <perharhine@mail.com                                                                            git config --global user.email perharhine@mail.com>
2022-10-21 07:57:54 -0700

    correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
  * @c: Variable text
  * Return: Always 0
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
@@ -16,6 +16,6 @@ int _isupper(int c);
 	
 	{
 
-		return (y);
+		return (0);
 	}
 }
