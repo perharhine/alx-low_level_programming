cc0bc0190cc3de268a9132f30bd2c1dc07662baf
Paul Erharhine <perharhine@mail.com                                                                            git config --global user.email perharhine@mail.com>
2022-10-26 14:03:24 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * @src: copy from
  * Return: pointer to dest
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -14,14 +14,14 @@ char *_strcat(char *dest, char *src);
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		i++
+		i++;
 	}
 		j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-			i++
-			j++
+			i++;
+			j++;
 	}
 		dest[i] = '\0';
 		return (dest);
