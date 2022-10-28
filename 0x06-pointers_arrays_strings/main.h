515a9ff1aee54baf1dfbfb67b7aa18eff0f1ee01
Paul Erharhine <perharhine@mail.com                                                                            git config --global user.email perharhine@mail.com>
2022-10-27 18:02:50 -0700

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,9 +6,9 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *a);
-char *cap_string(char *a);
-char *leet(char *a);
+char *string_toupper(char *n);
+char *cap_string(char *str);
+char *leet(char *n);
 int _putchar(char c);
 
 #endif
